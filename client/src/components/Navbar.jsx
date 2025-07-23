e29dbd42c4f001333e5b7a82d1211ff348e3dbc6
aditya-nagar <adityanagar979@gmail.com>
2025-07-23 11:53:32 +0530

    initial commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { data } from "../restApi.json";
+import { Link } from "react-scroll";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+const Navbar = () => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <nav>
+        <div className="logo">MERN Meal</div>
+        <div className={show ? "navLinks showmenu" : "navLinks"}>
+          <div className="links">
+            {data[0].navbarLinks.map((element) => (
+              <Link
+                to={element.link}        // This should match the 'id' of the section
+                spy={true}
+                smooth={true}
+                duration={500}
+                key={element.id}
+                offset={-70}             // Adjust scroll offset if header overlaps
+                onClick={() => setShow(false)} // Close menu on link click (optional)
+              >
+                {element.title}
+              </Link>
+            ))}
+          </div>
+
+          {/* FIX: Link 'OUR MENU' button to #menu section */}
+          <Link
+            to="menu"
+            spy={true}
+            smooth={true}
+            duration={500}
+            offset={-70}
+          >
+            <button className="menuBtn">OUR MENU</button>
+          </Link>
+        </div>
+
+        <div className="hamburger" onClick={() => setShow(!show)}>
+          <GiHamburgerMenu />
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
